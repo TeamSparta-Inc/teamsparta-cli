f7ae5e49fca12eac28da231183fd3383954c3449
admin <js.byeon@teamsparta.co>
2023-12-07 22:42:10 +0900

    add v flag

--- sprt/get.binary.js
+++ sprt/get.binary.js
@@ -2,7 +2,7 @@ const { Binary } = require("binary-install");
 
 function getBinary() {
   const version = require("../package.json").version;
-  const url = `https://github.com/TeamSparta-Inc/teamsparta-cli/releases/tag/${version}/sprt.tar.gz`;
+  const url = `https://github.com/TeamSparta-Inc/teamsparta-cli/releases/tag/v${version}/sprt.tar.gz`;
   const name = "sprt";
 
   return new Binary(name, url);
